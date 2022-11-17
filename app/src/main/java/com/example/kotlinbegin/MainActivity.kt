65b3f932a65aac77cc0d04b59a6cffe46b8804cf
Global_Developer <kayangemwakanemela@gmail.com>
2022-11-17 13:43:54 +0200

    kotlin_video5_section_2_implicit_intent

--- app/src/main/java/com/example/kotlinbegin/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbegin/MainActivity.kt
@@ -29,5 +29,16 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btnShareToOtherApps.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+
+            //implicit intent
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "(Mwaka app says) -> Share to : "))
+        }
+
     }
 }
