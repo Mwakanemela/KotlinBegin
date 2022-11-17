d832715cf272837400099bcdcb82418f5e88f82b
Global_Developer <kayangemwakanemela@gmail.com>
2022-11-17 12:49:22 +0200

    kotlin_video3_section_2_navigate_between_activities

--- app/src/main/java/com/example/kotlinbegin/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbegin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinbegin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,7 +14,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         btnShowToast.setOnClickListener {
             Log.i("MainActivity", "Button was clicked")
-            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Button click", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendToNextActivity.setOnClickListener {
+
+
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+
+
         }
 
     }
